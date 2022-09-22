77f2788004e5589538198b62b5507ec92e7b6f16
1E10 Laptop <sample@bths.edu>
2022-09-22 12:19:01 -0400

    tipCalculator assessment

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,16 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         System.out.println("Welcome to the tip calculator!");
         System.out.print("How many people are in your group? ");
-        String numCustomer = scan.nextLine();
+        int numCustomer = scan.nextInt();
         System.out.print("What's the tip percentage? (0-100): ");
         int percentage = scan.nextInt();
+        //use while loop to get each cost
+        int dish = 0;
+        while(dish != numCustomer ) {
+            System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1) to end: ")
+            dish++;
+        }
         scan.nextLine();
+
     }
 }
